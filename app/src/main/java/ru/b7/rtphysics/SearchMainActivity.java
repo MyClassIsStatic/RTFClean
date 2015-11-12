1d8fbf3108f47b12ab888ba3062605be83498afd
MyClassIsStatic <aifos.64@mail.ru>
2015-11-12 10:10:37 +0500

    Clean

---
+++ app/src/main/java/ru/b7/rtphysics/SearchMainActivity.java
@@ -0,0 +1,34 @@
+package ru.b7.rtphysics;
+
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.View;
+
+/**
+ * Search Activity. Navigation drawer enabled, back button goes to HandbookMenuActivity.
+ */
+public class SearchMainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    protected int getNavDrawerItem() {
+        return NAV_DRAWER_ITEM_SEARCH;
+    }
+
+    @Override
+    public void onBackPressed() {
+        NavUtils.navigateUpFromSameTask(this);
+
+
+    }
+}
